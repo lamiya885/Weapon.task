d4ed9e84452cd822bc036820acf12b72395c7d8c
lamiya885 <lamiyabh-bp215@code.edu.az>
2024-10-18 23:18:19 +0400

    First commit

---
+++ Program.cs
@@ -0,0 +1,98 @@
+﻿
+
+using System.Runtime.CompilerServices;
+using ConsoleApp;
+
+namespace ConsoleApp5
+
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            bool f = false;
+
+            int GulleSayi;
+            int GulleninLimiti;
+            bool AtmaqucunMod;
+
+            
+
+            Weapon weapon = new Weapon();
+            do
+            {
+                Console.WriteLine("0:Informasiya almaq ucun;1:shoot metodu ucun;2:GetRemainBulletCount metodu ucun;3:Reload metodu ucun ; 4:ChangeFireMode metodu ucun;5:proqramdan dayandirmaq ucun");
+                int operation;
+                int.TryParse(Console.ReadLine(), out operation);
+
+
+
+                switch (operation)
+                {
+                    case 0:
+
+                        do
+                        {
+                            Console.WriteLine("gulle sayi daxil edin");
+                            bool a= int.TryParse(Console.ReadLine(),out GulleSayi);
+                            if(a=true)
+                            {
+                                Console.WriteLine("daxil etdiyiniz deyer serti odeyir . zehmet olmasa hemin deyeri yeniden daxil edin");
+                                int.Parse(Console.ReadLine());
+                            }
+                       
+                            Console.WriteLine("gulle limiti daxil edin");
+                            
+                                bool b = int.TryParse(Console.ReadLine(), out GulleninLimiti);
+
+                                if (b = true)
+                                {
+                                    Console.WriteLine("daxil etdiyiniz deyer serti odeyir . zehmet olmasa hemin deyeri yeniden daxil edin");
+
+                                    int.Parse(Console.ReadLine());
+
+                                }
+                           
+                            Console.WriteLine("1 defeye nece gulle atir:");
+                            bool.TryParse(Console.ReadLine(), out AtmaqucunMod);
+
+                        } while (false);
+                        weapon = new Weapon(GulleninLimiti, GulleSayi, AtmaqucunMod);
+
+
+
+                        break;
+
+                    case 1:
+                        weapon.Shoot();
+                        break;
+
+                    case 2:
+                        weapon.Reload();
+                        break;
+
+                    case 3:
+                        weapon.GetRemainBulletCount();
+                        break;
+
+                    case 4:
+                        weapon.ChangeFireMode();
+                        break;
+
+                    case 5:
+                        f = true;
+                        break;
+
+
+                    default:
+                        Console.WriteLine("yuxaridaki ededlerden daxil edin");
+                        break;
+
+                }
+            } while (!f);
+
+
+        }
+    }
+}
---
+++ weapon.cs
@@ -0,0 +1,131 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+
+
+    namespace ConsoleApp
+    {
+        internal class Weapon
+        {
+            int _gulleLimit;
+            public int GulleLimit
+            {
+                get
+                {
+                    return _gulleLimit;
+
+                }
+                set
+                {
+                    if (value > 0)
+                    {
+
+                        _gulleLimit = value;
+                    }
+                }
+            }
+
+
+            int _gulleNum;
+            public int GulleNum
+            {
+                get
+                {
+                    return _gulleNum;
+                }
+                set
+                {
+                    if (value > 0 && value < GulleLimit)
+                    {
+                        _gulleNum = value;
+                    }
+                }
+            }
+
+            bool _atisModu;
+            bool tekliAtis=true;
+
+            public bool AtisModu
+            {
+                get
+                {
+                    return _atisModu;
+                }
+                set
+                {
+                    if (_atisModu =tekliAtis)
+                    {
+
+                        Console.WriteLine("atis modu:avtomatik");
+
+                    }
+                    else
+                    {
+
+                        Console.WriteLine("atis modu:tekli atis");
+                    }
+                    _atisModu = value;
+
+                }
+            }
+            public Weapon(int gullelimiti, int gullenum, bool atismodu)
+            {
+                GulleLimit = gullelimiti;
+                GulleNum = gullenum;
+                AtisModu = atismodu;
+            }
+
+            public Weapon()
+            {
+
+            }
+
+            public void Shoot()
+            {
+                if (tekliAtis = true)
+                {
+                    GulleNum--;
+
+                    Console.WriteLine($"gulle sayi:{GulleNum}", tekliAtis);
+                }
+
+                else
+                {
+                    GulleNum = 0;
+                    Console.WriteLine($"gulle sayi:{GulleNum} ", tekliAtis);
+                }
+            }
+            public int GetRemainBulletCount()
+            {
+                int a = GulleLimit - GulleNum;
+                Console.WriteLine($"  daragin tam dolmasi ucun lazim olan gulle sayi:{a},gulle sayi:{GulleNum}");
+                return a;
+            }
+            public void Reload()
+            {
+                int a = GulleLimit - GulleNum;
+                GulleNum += a;
+
+            }
+            public void ChangeFireMode()
+            {
+                if (tekliAtis )
+                {
+                    tekliAtis=!tekliAtis;
+                    Console.WriteLine(" tekli atis modundadir");
+                }
+                else
+                {
+                    tekliAtis=tekliAtis ;
+                    Console.WriteLine("avtomatik modundadir");
+                }
+
+            }
+        }
+    }
+
+
